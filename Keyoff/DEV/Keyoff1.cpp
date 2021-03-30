1a4384f1335f7fadbe3d65031148d989c414e8f5
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-29 18:18:23 -0700

    KeyOff1.cpp
    The main script for this project.

---
+++ Keyoff/DEV/Keyoff1.cpp
@@ -0,0 +1,60 @@
+// Start of script
+#include <iostream>
+#include <fstream> 
+#include <vector> 
+#include <string> 
+// The main script for Keyoff.
+// Please note that I am not very experienced with C++ so this is just a demo script for now, and is not yet functional
+class keyMapMain {
+  int main()
+  {
+      std::cout << "Keyoff main";
+      std::Cout << "<QML Text>";
+      break;
+  }
+  void keySelect()
+  {
+    std::cout << "Select which key?"; // QWERTY keyboard is used as demo, specifically my laptops keyboard, which varies from other keyboards
+    keyListRow1 = ["ESCAPE" "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "INSERT", "DELETE", "POWER"];
+    keyListRow2 = ["~`", "!1", "@2", "#3", "$4", "%5", "^6", "&7", "*8", "(9". ")0", "_-", "+=", "BACKSPACE"];
+    keyListRow3 = ["TAB", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "{[", "}]", "|\"];
+    keyListRow4 = ["CAPS LOCK", "A", "S", "D", "F", "G", "H", "J", "K", "L", ":;" "\"'", "ENTER"];
+    keyListRow5 = ["SHIFT", "Z", "X", "C", "V", "B", "N", "M", "<,", ">.", "?/" "SHIFT"];
+    keyListRow6 = ["CONTROL", "FUNCTION", "SUPERKEY", "ALT", "SPACE", "ALT", "CONTROL", "LEFT", "UP/PAGEUP", "DOWN/PAGEDOWN", "RIGHT"];
+    break;
+    // For GitHub, for some reason }}
+                  // Nope, didn't fix the tab issue. I was hoping that it would have fixed it, despite being commented, as GitHub grabbed 2 curly brackets that were in string format
+  }
+  void vmFullScreen()
+                   {
+                     // Isn't at all functional yet
+                     int xtimer = "300000";
+                     int xTimerTest = "300"
+                     int xTimerSeconds = "300";
+                     int xTimerTestSeconds = "3";
+                     override System for xTimerTest;
+                     display std::cout >> "Keyboard is off for" + str(xTimerTestSeconds) + " seconds";
+                     break;
+                   }
+using namespace std;
+vector<double> importdata(string KEYMAP.asm) 
+{ 
+	ifstream fin(../MAP/KEYMAP.asm); 
+	vector<double> data; 
+	double tmp; 
+	while(fin>>tmp) 
+		data.push_back(tmp); 
+	fin.close(); 
+	return data;
+  break;
+}
+// Source: https://qr.ae/pGR5IE (Quora)
+// Now more of my work
+
+}
+/* File info
+* File version: 1 (Monday, March 29th 2021 at 6:18 pm)
+* File type: C++ Source file (*.cpp)
+* Line count (including blank lines and compiler line): 63
+*/
+// End of script
